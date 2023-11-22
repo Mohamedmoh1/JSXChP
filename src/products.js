c73db9e19db78c18f068f9803c05e7a3bae8b010
Mohamedmoh1 <med24795@gmail.com>
2023-11-22 12:05:40 +0100

    JSX

---
+++ src/products.js
@@ -0,0 +1,9 @@
+var products = {
+    "name" : "Hoodie",
+    "description" : "549 Phantom Hoodie",
+    "price" : 48,
+    "URL" : "https://p16-sign-va.lemon8cdn.com/tos-maliva-v-ac5634-us/269665d31b724f4b974d861b9d8b2b23~tplv-tej9nj120t-shrink:1080:0:q50.webp?source=seo_feed_list&x-expires=1729080000&x-signature=XWdROhhXoAKs%2FHD%2Bs0aKBIhd7zU%3D"
+}
+
+
+export default products
